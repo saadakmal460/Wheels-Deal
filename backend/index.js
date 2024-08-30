f1ddf2fda9bbe425e3a8e294d61e2acb5297a590
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-08-30 17:51:54 +0500

    Changes

--- backend/index.js
+++ backend/index.js
@@ -3,41 +3,33 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = require('./config');
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const router = require('./Routes/Router');
-const {Logger} = require('./Middleware/logger')
-const path = require('path')
+const { Logger } = require('./Middleware/logger');
+const path = require('path');
+
 dotenv.config();
 
-const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(cors());
 
-
-app.use('/api' , router);
-
+app.use('/api', router);
 app.use(Logger);
-
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
+});
 
-mongoose
-.connect(process.env.MONGO)
-.then(()=>{
-
+mongoose.connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to DB");
-    app.listen(PORT , ()=>{
-        console.log("App is listening to port " + PORT);
-    })
-
-})
-.catch((error)=>{
+    app.listen(PORT, () => {
+      console.log("App is listening to port " + PORT);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-})
-
+  });
